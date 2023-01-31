db2eef596a15b0c8e763253786b69aff523cf5a0
Jarrod Watts <jarrodwatts16@gmail.com>
2023-01-31 14:31:47 +1100

    initial commit

---
+++ application/pages/index.tsx
@@ -0,0 +1,55 @@
+import { ConnectWallet } from "@thirdweb-dev/react";
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
+        </h1>
+
+        <p className={styles.description}>
+          Get started by configuring your desired network in{" "}
+          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
+          <code className={styles.code}>pages/index.tsx</code> file!
+        </p>
+
+        <div className={styles.connect}>
+          <ConnectWallet />
+        </div>
+
+        <div className={styles.grid}>
+          <a href="https://portal.thirdweb.com/" className={styles.card}>
+            <h2>Portal &rarr;</h2>
+            <p>
+              Guides, references and resources that will help you build with
+              thirdweb.
+            </p>
+          </a>
+
+          <a href="https://thirdweb.com/dashboard" className={styles.card}>
+            <h2>Dashboard &rarr;</h2>
+            <p>
+              Deploy, configure and manage your smart contracts from the
+              dashboard.
+            </p>
+          </a>
+
+          <a
+            href="https://portal.thirdweb.com/templates"
+            className={styles.card}
+          >
+            <h2>Templates &rarr;</h2>
+            <p>
+              Discover and clone template projects showcasing thirdweb features.
+            </p>
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
