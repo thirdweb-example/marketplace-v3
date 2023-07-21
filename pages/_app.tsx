0e3362f69155668549afc20f04a65c010aad6ff7
Atharva Deosthale <atharva.deosthale17@gmail.com>
2023-07-21 16:55:04 +0530

    update nev

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
+      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={NETWORK}
     >
       {/* Progress bar when navigating between pages */}
